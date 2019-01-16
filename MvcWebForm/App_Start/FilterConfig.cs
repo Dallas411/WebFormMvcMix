ec3ec7241c78720686426fcfc294c6886c5488c4
Marco Dalla Libera <marco@dallalibera.net>
2019-01-16 12:14:16 +0100

    Aggiungere i file di progetto.

---
+++ MvcWebForm/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebForm
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
